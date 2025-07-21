cb12b9899ac977dbaffb2e01b4a90d18d4796242
dong <dho0647@naver.com>
2025-07-22 06:00:57 +0900

    WebFlux의 Subscriber - Publisher 패턴

--- src/main/java/com/example/webflux/controller/ReactiveProgrammingExampleController.java
+++ src/main/java/com/example/webflux/controller/ReactiveProgrammingExampleController.java
@@ -1,15 +1,18 @@
 package com.example.webflux.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/reactive")
+@Slf4j
 public class ReactiveProgrammingExampleController {
 
     // 1~9까지 출력하는 API
@@ -28,16 +31,17 @@ public class ReactiveProgrammingExampleController {
 
     @GetMapping("/onenine/flux")
     public Flux<Integer> produceOneToNineFlux() {
-        return Flux.create(sink -> {
+        return Flux.<Integer>create(sink -> {
             for (int i = 1; i <= 9; i++) {
                 try {
+                    log.info("현재 처리하고 있는 스레드 이름 : {}", Thread.currentThread().getName()); // reactor-http-nio-4 스레드는 블로킹 되지 않게 만들어야함.
                     Thread.sleep(500);
                 } catch (Exception e) {
                 }
                 sink.next(i);
             }
             sink.complete();
-        });
+        }).subscribeOn(Schedulers.boundedElastic());
     }
     // 리액티브 스트링 구현체 Flux, Mono를 사용하여 발생하는 데이터를 바로바로 리액티브하게 처리
     // 비동기로 동작 - 논 블로킹하게 동작 해야한다. Thread는 블로킹 코드이기 때문에 우회가 필요하다.
---
+++ src/test/java/com/example/webflux/SubscriberPublisherAsyncTest.java
@@ -0,0 +1,55 @@
+package com.example.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@SpringBootTest
+public class SubscriberPublisherAsyncTest {
+
+    // 한글로 구독자 - 발행자
+    // Flux를 구독하면 발행이 시작된다.
+    // 이 간단한 개념이 Subscriber - Publisher 패턴이다.
+    @Test
+    public void produceOneToNineFlux() {
+        Flux<Integer> intFlux = Flux.<Integer>create(sink -> {
+            for (int i = 1; i <= 9; i++) {
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                }
+                sink.next(i);
+            }
+            sink.complete();
+        }).subscribeOn(Schedulers.boundedElastic());
+
+        // 스레드 1개만 사용해서는 절대로 블로킹을 회피할 수 없다.
+        // Reactor의 스케쥴러를 사용해서 스레드를 추가 할당해야지만 블로킹 회피가 가능하다.
+        // 스케쥴러가 제공하는 스레드는 톰켓의 스레드처럼 어느정도 블로킹 되어도 괜찮다.
+        // 이 스레드는 우리가 원하는 곳에 마음대로 사용할 수 있다.
+        // 스케쥴러가 제공하는 스레드가 중요한 스레드 (이벤트 루프 스레드) 대신 대기 하는것이 블로킹 회피의 기본적인 전략이다.
+        intFlux.subscribe(data -> {
+            System.out.println("처리 되고 있는 스레드 이름 : " + Thread.currentThread().getName());
+            System.out.println("WebFlux가 구독 중!! : " + data);
+        });
+        System.out.println("Netty 이벤트 루프로 스레드 복귀 !!");
+
+        // 테스트 환경이라 메인 스레드는 자기 할 일을 다하면 죽어버린다.
+        // 메인 스레드가 살아 있어야지만 다른 스레드들도 일을 하고 있을 수 있다.
+        // 그래서 테스트를 위해 메인 스레드를 잠시 잡아준다.
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+        }
+    }
+
+    // 정리
+    /**
+     * 1. Flux에는 데이터가 들어 있는게 아니라 함수 코드 덩어리가 들어있다.
+     * 그래서 함수를 실행시키는 순간 부터 데이터가 발행된다.
+     * 2. 스레드가 1개라면 Flux만으로는 어떻게 해도 블로킹을 회피할 수 없다.
+     * 스케쥴러로 추가 스레드를 할당하여 대신 작업시켜야 한다.
+     */
+
+}
